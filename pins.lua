d04752f451a61ebdcf2418db250ab0ebe1dae0db
ryanjblajda <ryan.j.blajda@gmail.com>
2025-11-18 14:30:23 -0500

    feat: create audio pins for external connections

---
+++ pins.lua
@@ -0,0 +1,35 @@
+table.insert(pins,
+{
+  Name = "Input A - Dry Feed",
+  Direction = "input",
+})
+
+table.insert(pins,
+{
+  Name = "Input B - Dry Feed [Optionally Post AEC Processing]",
+  Direction = "input",
+})
+
+table.insert(pins,
+{
+  Name = "Input C - Dry Feed [Optionally Post AEC Processing]",
+  Direction = "input",
+})
+
+table.insert(pins,
+{
+  Name = "Output A - Wet",
+  Direction = "output",
+})
+
+table.insert(pins,
+{
+  Name = "Output B - Pre-Fader (Post Mute)",
+  Direction = "output",
+})
+
+table.insert(pins,
+{
+  Name = "Output C - Pre-Fader (Post Mute)",
+  Direction = "output",
+})
