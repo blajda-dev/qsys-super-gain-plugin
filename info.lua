c4e48c108bf664f1add800b2cf31f2733549ac0c
ryanjblajda <ryan.j.blajda@gmail.com>
2025-11-18 14:28:02 -0500

    chore: plugin information

---
+++ info.lua
@@ -0,0 +1,8 @@
+PluginInfo = {
+  Name = "RBLAJDA~Super Gain",
+  Version = "1.0",
+  BuildVersion = "1.0.0.2",
+  Id = "eadbc6a2-3b3c-4cf3-bd88-09273d1ad11f",
+  Author = "rblajda",
+  Description = "a better verison of the gain object"  
+}
