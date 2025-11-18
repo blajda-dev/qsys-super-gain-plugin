198a725aa5f766f9f1757e05b4b82281d77dd219
ryanjblajda <ryan.j.blajda@gmail.com>
2025-11-18 14:33:54 -0500

    feat: create the connections between the created pins and the generated components

---
+++ wiring.lua
@@ -0,0 +1,39 @@
+--[full wet]
+
+table.insert( wiring, 
+{ 
+  "Input A - Dry Feed", 
+  "Main Input" 
+})
+
+table.insert( wiring, 
+{ 
+  "Main Output", 
+  "Output A - Wet" 
+})
+
+--[pre-fader listen post-mute]
+
+table.insert( wiring, 
+{ 
+  "Input B - Dry Feed [Optionally Post AEC Processing]", 
+  "Passthrough Input Channel 1" 
+})
+
+table.insert( wiring, 
+{ 
+  "Input C - Dry Feed [Optionally Post AEC Processing]", 
+  "Passthrough Input Channel 2" 
+})
+
+table.insert( wiring, 
+{ 
+  "Passthrough Output Channel 1",
+  "Output B - Pre-Fader (Post Mute)"
+})
+
+table.insert( wiring, 
+{ 
+  "Passthrough Output Channel 2",
+  "Output C - Pre-Fader (Post Mute)"
+})
